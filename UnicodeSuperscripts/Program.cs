e13c3afd5ae2c887d0cd005f13efe966b9a01376
mypikachu <pikachu.0927.yng@gmail.com>
2022-06-21 16:50:06 +0800

    FirstCommit

---
+++ UnicodeSuperscripts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UnicodeSuperscripts
+{
+    //Unicode list
+    //https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u2070.html
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("CO\u2082e");//CO2e
+            Console.ReadLine();
+        }
+    }
+}
